afd5ffe5e259e092a125f269cc273c3d6b549c68
SuryanshChourasiya <suryanshchourasiya9993@gmail.com>
2024-05-02 19:33:00 +0530

    Query param

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 
 App = Flask(__name__)
 
@@ -8,7 +8,7 @@ ideas = {
         "id": 1,
         "idea_name": "Generate the random data ",
         "idea_disct": "paas",
-        "idea_author": "Suryansh",
+        "idea_author": "Rahul",
     },
     2: {
         "id": 2,
@@ -26,28 +26,56 @@ create an restful endpoint for fetecing the ideas
 @App.get("/ideaapp/api/v1/ideas")
 def get_all_ideas():
     # logic to fetech all the ideaa
+    # I need to read quairy param
+    idea_author = request.args.get("idea_author")
+    # Logic to fetch all the ideas and support query params
+    if idea_author:
+        # Filter the ideas created by this author
+        idea_res = {}
+        for key, values in ideas.items():
+            if values["idea_author"] == idea_author:
+                idea_res[key] = values
+        return idea_res
     return ideas
+
 
 """
 Create the RESTful endpoint for creating the ideas 
 """
+
+
 @App.post("/ideaapp/api/v1/ideas")
 def create_idea():
     try:
-        request_body=request.get_json()
+        request_body = request.get_json()
 
         if request_body["id"] and request_body["id"] in ideas:
-            return "ideas with the same id is already exists",400 
-        
-        ideas[request_body["id"]]=request_body
-        
-        return "created and saved successfully ",201
+            return "ideas with the same id is already exists", 400
+
+        ideas[request_body["id"]] = request_body
+
+        return "created and saved successfully ", 201
     except KeyError:
         return "id key is missing "
-    
-    except :
+
+    except:
         return "internal error"
-    
+
+
+"""
+End point to fetch idea based on the idea id
+"""
+
+
+@App.get("/ideaapp/api/v1/ideas/<idea_id>")
+def get_idea_id(idea_id):
+    try:
+        if int(idea_id) in ideas:
+            return ideas[int(idea_id)], 200
+        else:
+            return "idea id passed in not present ", 400
+    except:
+        "some internal errror happend", 500
 
 
 if __name__ == "__main__":
